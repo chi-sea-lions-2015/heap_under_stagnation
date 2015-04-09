b358aa17020e962d4d34b7eb2ef1b7b7db92fff7
Taylor Masterson <taylor.j.masterson@gmail.com>
2015-04-09 17:36:23 -0500

    writing seeds

--- HUS_app/db/seeds.rb
+++ HUS_app/db/seeds.rb
@@ -1,2 +1,9 @@
 user1 = User.create(username: "paul", email: "paul@paul.com", password: "12345")
 user2 = User.create(username: "sara", email: "sara@sara.com", password: "12345")
+user3 = User.create(username: "taylor", email: "taylor@taylor.com", password: "12345")
+user4 = User.create(username: "chase", email: "chase@chase.com", password: "12345")
+
+10.times do |n|
+  samples = User.all.sample(11)
+  asker = samples.pop
+  Question.create(title: Faker::Lorem.word)
