7481ee4937835e47695279ba9fbc0cd1c39dfdf6
Taylor Masterson <taylor.j.masterson@gmail.com>
2015-04-09 14:16:28 -0500

    some ass

--- HUS_app/app/models/question.rb
+++ HUS_app/app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   has_many :votes, as: :voteable
   has_many :comments, as: :commentable
   has_many :answers
+  belongs_to :asker, class_name: "User"
 end
--- HUS_app/app/models/user.rb
+++ HUS_app/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :questions
+  has_many :answers
+  has_many :comments, as: :commentable
+  has_many :votes, as: :voteable
   include BCrypt
 
   def password
