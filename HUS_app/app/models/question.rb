e936351f53170234e75434e097932f6fe1cb0759
Xiyao Chen & Paul Clegg <pair+chenxiyao.cleggameg@devbootcamp.com>
2015-04-10 11:01:59 -0500

    Complete add answers and showing of answers both using partials :)

--- HUS_app/app/models/question.rb
+++ HUS_app/app/models/question.rb
@@ -4,6 +4,6 @@ class Question < ActiveRecord::Base
   has_many :answers
   belongs_to :asker, class_name: "User", foreign_key: "user_id"
 
-  validates :title, presence: true, length: { in: 5..50}
+  validates :title, presence: true, length: { in: 1..50}
   validates :content, presence: true
 end
