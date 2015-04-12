71bf28168afa1061ece9d817520544ad781bb60f
cleggameg <cleggameg@gmail.com>
2015-04-12 18:28:27 -0500

    FINALLY ALL GOgit add .

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,30 +1,19 @@
 $(document).ready(function() {
 
   $(".new_comment").on("ajax:success", function(e, data, status, xhr) {
-  $(".question_comment_list").append(xhr.responseText);
-  $(".comment_text_area").val("")
-  return console.log("stuff", xhr.responseText);
-}).on("ajax:error", function(e, xhr, status, error) {
-  return $("#new_comment").prepend("<p>ERROR</p>");
-});
+    $(".question_comment_list").append(xhr.responseText);
+    $(".comment_text_area").val("")
+    return console.log("stuff", xhr.responseText);
+  }).on("ajax:error", function(e, xhr, status, error) {
+    return $("#new_comment").prepend("<p>ERROR</p>");
+  });
 
 
 
   $(".new_answer_comment").on("ajax:success", function(e, data, status, xhr) {
-    var answerCommentList = $(this).parent().parent().find(".answer_comment_list");
-    answerCommentList.append(xhr.responseText);
+    $(".answer_comment_list").append(xhr.responseText);
     $(".comment_text_area").val("");
   });
-
-<<<<<<< HEAD
-  $(".show_answer_comments_form").on("click", function(event){
-    var form_class = $(this).attr("href");
-    console.log("HEYYYYYYYYYYYYY");
-    event.preventDefault();
-    $("#" + form_class).css("display", "block")
-  })
-
-=======
 
   $(".show_answer_comments_form").on("click", function(event){
     var form = $(this).parent().find(".answer_comment");
@@ -32,5 +21,5 @@ $(document).ready(function() {
     event.preventDefault();
     form.css("display", "block");
   });
->>>>>>> 2ee34090ad3dcb747690ad553e7216b825d9a5c9
+
 });
