fb4a205d70f7139b79c41db55e2fc55e1a33ef78
Xiyao Chen <sarachenxiyao@hotmail.com>
2015-04-12 13:50:59 -0500

    add answer commit form

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -8,4 +8,14 @@ $(document).ready(function() {
   return $("#new_comment").prepend("<p>ERROR</p>");
 });
 
+
+
+  $(".new_answer_comment").on("ajax:success", function(e, data, status, xhr) {
+    $(".question_comment_list").append(xhr.responseText);
+    $(".comment_text_area").val("")
+    return console.log("stuff", xhr.responseText);
+}).on("ajax:error", function(e, xhr, status, error) {
+  return $("#new_comment").prepend("<p>ERROR</p>");
+});
+
 });
